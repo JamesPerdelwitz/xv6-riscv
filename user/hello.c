73888bcac0d1811ecd9d0c3ac9f8b69e63d5d1cf
James Perdelwitz <perdej1@unlv.nevada.edu>
2025-09-05 19:45:51 -0700

    My solution for Project 1 on xv6

---
+++ user/hello.c
@@ -0,0 +1,11 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main() 
+{
+	printf("Hello world, this is James Perdelwitz.\n");
+	printf("I am in CS 370, Fall 2025, section 1003.\n\n");
+
+	exit(0);
+}
